38e1946e244556194a36e891796765b870f88b1c
Sydney Medina <sydneymedina5@gmail.com>
2025-07-20 01:13:56 +0000

    devcontainer

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
-import uvicorn
 import logging
+import uvicorn
 from fastapi import FastAPI
 
 
 def main():
+    """Runner code for Ordering Book App"""
+    
     logging.info("Order book engine running")
     app = FastAPI()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
